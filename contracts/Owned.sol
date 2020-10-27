a81557ecb9209d6ca9c1b9ad62f75105dc1c04c1
sahar panahloo <sahar.aplo@gmail.com>
2020-10-28 00:15:08 +0330

    Added NatSpec comments to the smart contracts

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
+/// @title Store the contract owner address 
+/// @author Sahar A.Panahloo
 contract Owned{
     
     address payable public  owner;
@@ -11,9 +13,12 @@ contract Owned{
     }
     
 }
-
+/// @title Destroy the smart contract when needed by the owner
+/// @author Sahar A.Panahloo
 contract Mortal is Owned{
     
+    /// @notice Destroy the smart contract
+    /// @dev Destroy the smart contract using selfdestruct() function that returns all funds to the owner address
     function kill() public{
         
         require(msg.sender==owner);
--- contracts/freelancing.sol
+++ contracts/freelancing.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 
 import "./SafeMath.sol";
 import "./Owned.sol";
+
 /// @title A platform for eployers and freelancers
 /// @author Sahar A.Panahloo
 contract freelancing is Owned, Mortal{
@@ -30,6 +31,9 @@ modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 modifier open(uint _id){require(projects[_id].status==Status.Open);_;}
 modifier done(uint _id){require(projects[_id].status==Status.Done);_;}
 
+/// @notice Add a project as an employer
+/// @dev Using SafeMath library for preventing overflow vulnerability
+/// @param _name The name of the project
 function addProject(string memory _name) public payable{
   
 require(msg.value>0);
@@ -40,6 +44,9 @@ projects[_id] = Project({id:_id,name:_name,price:msg.value,status:Status.Open,em
 emit AddProject(_id,_name,msg.value);
 
 }
+/// @notice Pick an open project as a freelancer
+/// @param _id The project id
+/// @return The project id
 function pickProject(uint _id)public open(_id) returns(uint){
 
 projects[_id].freelancer=msg.sender;
@@ -47,11 +54,18 @@ projects[_id].status=Status.NotDone;
 emit PickProject(_id,msg.sender);
 return _id;
 }
+
+/// @notice Commit the project that you picked as a freelancer
+/// @param _id The project id
 function commitProject(uint _id)public verifyCaller(projects[_id].freelancer){
   
   projects[_id].status=Status.Done;
   emit CommitProject(_id,msg.sender); 
 }
+
+/// @notice Close the commited project as an employer, Ethers will be sent to the freelancer 
+/// @dev Sending Ethers with call function and check the result
+/// @param _id The project id
 function closeProject(uint _id)public payable done(_id) verifyCaller(projects[_id].employer){
 
 projects[_id].status=Status.Closed;
@@ -62,6 +76,16 @@ if(!success) {
 }
 emit CloseProject(_id,projects[_id].employer,projects[_id].freelancer,projects[_id].price);
 }
+
+/// @notice Get the project specifications
+/// @dev Solidity doesnt support the struct return type in public functions
+/// @param _id The project id
+/// @return The project name
+/// @return The project id
+/// @return The project price
+/// @return The project status
+/// @return The employer address
+/// @return The freelancer address
 function returnProject(uint _id)public view returns(string memory name,uint id,uint price,uint status,address employer,address freelancer){
 
 name=projects[_id].name;
