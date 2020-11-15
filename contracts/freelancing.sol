c0b30b0c2624cdbeff240674dca59044eacdd517
sahar panahloo <sahar.aplo@gmail.com>
2020-11-15 21:39:42 +0330

    The Circuit breaker design pattern added

--- contracts/freelancing.sol
+++ contracts/freelancing.sol
@@ -9,6 +9,8 @@ import "./Owned.sol";
 contract Freelancing is Owned, Mortal{
 using SafeMath for uint256;
 
+bool public stopped=false;
+
 struct Project{
   uint    id;
   string  name;
@@ -26,15 +28,26 @@ event AddProject(uint projectId,string name,uint price);
 event PickProject(uint projectId,address freelancer);
 event CommitProject(uint projectId,address freelancer);
 event CloseProject(uint projectId,address employer,address freelancer,uint price);
+event Withdraw(address employer,uint price);
 // modifiers
 modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 modifier open(uint _id){require(projects[_id].status==Status.Open);_;}
 modifier done(uint _id){require(projects[_id].status==Status.Done);_;}
+// circuit breaker pattern modifiers
+modifier stopInEmergency { require(!stopped); _; }
+modifier onlyInEmergency { require(stopped); _; }
+modifier onlyOwner{require(msg.sender==owner);_;}
 
+/// @notice Stopp contract functionality in case of a bug is detected
+/// @dev Using the Circuit braker design pattern
+/// @param _stopped boolian state variable for stopping the contract
+function breakCircuit(bool _stopped)public onlyOwner{
+    stopped=_stopped;
+} 
 /// @notice Add a project as an employer
 /// @dev Using SafeMath library for preventing overflow vulnerability
-/// @param _name The name of the project
-function addProject(string memory _name) public payable{
+/// @param _name Name of the project
+function addProject(string memory _name) public payable stopInEmergency{
   
 require(msg.value>0);
 uint _id=numProjects;
@@ -47,7 +60,7 @@ emit AddProject(_id,_name,msg.value);
 /// @notice Pick an open project as a freelancer
 /// @param _id The project id
 /// @return The project id
-function pickProject(uint _id)public open(_id) returns(uint){
+function pickProject(uint _id)public open(_id) stopInEmergency returns(uint){
 
 projects[_id].freelancer=msg.sender;
 projects[_id].status=Status.NotDone;
@@ -73,12 +86,13 @@ projects[_id].status=Status.Closed;
 if(!success) {
     // handle failure code
   projects[_id].status=Status.Done;  
-}
+}else{
 emit CloseProject(_id,projects[_id].employer,projects[_id].freelancer,projects[_id].price);
+}
 }
 
 /// @notice Get the project specifications
-/// @dev Solidity doesnt support the struct return type in public functions
+/// @dev Solidity doesnt support the struct return type in public function calls
 /// @param _id The project id
 /// @return The project name
 /// @return The project id
@@ -95,6 +109,21 @@ status=uint(projects[_id].status);
 employer=projects[_id].employer;
 freelancer=projects[_id].freelancer;
 }
+
+/// @notice Withdraw funds to the employer in an emergency
+/// @dev Set the price to zero and the status of the project to Closed 
+/// @param _id The project id
+function withdraw(uint _id)public open(_id) verifyCaller(projects[_id].employer) onlyInEmergency{
+
+uint price=projects[_id].price;
+projects[_id].price=0;
+projects[_id].status=Status.Closed;
+msg.sender.transfer(price);
+(bool success, ) = msg.sender.call.value(price)("");
+require(success);
+emit Withdraw(msg.sender,price);
+}
+
 
 }
 
